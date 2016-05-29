27b1e306e11f50c042f762806b2d88be4a3080f2
ukhellfire <ukhellfire666@live.co.uk>
2016-05-29 22:27:15 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(curl -s  https://raw.githubusercontent.com/AICP/vendor_jenkins/mm6.0/aicp-build-targets | sed -e 's/#.*$//' | grep mm6.0 | awk {'print $2'})
+for combo in $(curl -s  https://raw.githubusercontent.com/ukhellfire/vendor_jenkins/mm6.0/aicp-build-targets | sed -e 's/#.*$//' | grep mm6.0 | awk {'print $2'})
 do
     add_lunch_combo $combo
 done
